46ba90f43be12349696ca728a4f8fc2207947582
streamline-autmations <streamline.automations.hq@gmail.com>
2025-07-23 08:53:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src', // Tell Vite to treat /src as the project root
   plugins: [react()],
+  build: {
+    outDir: '../dist', // Output still goes to root-level dist
+    emptyOutDir: true, // Clears old build before building
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
